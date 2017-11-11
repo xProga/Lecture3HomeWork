2dd1f3eabbe289408292e5a9c4a6c14a79fc1345
Vadim Proga Krylov <progavs@gmail.com>
2017-11-11 15:39:38 +0500

    first commit

---
+++ Lecture3HomeWork/ClassCode.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3HomeWorkClasses
+{
+    struct Complex
+    {
+        public double im; //Комплексная часть
+        public double re; //Действительная часть
+        
+        /// <summary>
+        /// Сложение комплексных чисел
+        /// </summary>
+        /// <param name="x">Слагаемое складываемое с исходным значением</param>
+        /// <returns></returns>
+        public Complex Plus(Complex x)
+        {
+            Complex y;
+            y.im = im + x.im; // x1 + x2
+            y.re = re + x.re; // y1 + y2
+            return y;
+        }
+        
+        /// <summary>
+        /// Умножение комплексных чисел
+        /// </summary>
+        /// <param name="x">Множитель умножающий исходное значение</param>
+        /// <returns></returns>
+        public Complex Multi(Complex x)
+        {
+            Complex y;
+            y.im = im * x.im - re * x.re;
+            y.re = im * x.re + re * x.im;
+            return y;
+        }
+
+        /// <summary>
+        /// Вычитание комплексных чисел
+        /// </summary>
+        /// <param name="x">Вычитаемое из исходного значение</param>
+        /// <returns></returns>
+        public Complex Minus(Complex x)
+        {
+            Complex y;
+            y.im = im - x.im;
+            y.re = re - x.re;
+            return y;
+        }
+
+        /// <summary>
+        /// Деление комплексных чисел
+        /// </summary>
+        /// <param name="x">Делитель исходного комплексного числа</param>
+        /// <returns></returns>
+        public Complex Division(Complex x)
+        {
+            Complex y;
+            y.im = (im * x.im + re * x.re) / (Math.Pow(x.im, 2) + Math.Pow(x.re, 2));
+            y.re = (x.im * re - im * x.re) / (Math.Pow(x.im, 2) + Math.Pow(x.re, 2));
+            return y;
+        }
+
+        public override string ToString()
+        {
+            return re + "+" + im + "i";
+        }
+    }
+
+    public class Task2
+    {
+        /// <summary>
+        /// Метод поределения и вывода всех нечетных положительных чисел.
+        /// </summary>
+        public static void InputValuesUntilNotZero()
+        {
+            int inputValue;
+            int summ = 0;
+            bool flag = false;
+
+            do
+            {
+                Console.Write("Введите число (для завершеения введите 0): ");
+                try
+                {
+                    flag = Int32.TryParse(Console.ReadLine(), out inputValue);
+                    if (inputValue % 2 != 0 && inputValue > 0)
+                    {
+                        summ += inputValue;
+                    }
+                    else
+                    {
+                        summ += inputValue;
+                        flag = true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Введено некорректное значение!!!");
+                }
+            }
+            while (flag != true);
+
+            Console.WriteLine("Сумма всех введенных положительных нечетных чисел: {0}", summ);
+            Console.ReadKey();
+        }
+    }
+
+
+    /// <summary>
+    /// Класс дробей
+    /// </summary>
+    public class Fraction
+    {
+        public int upperValue; //Числитель
+        public int lowValue; //Знаменатель
+
+        public string toString()
+        {
+            return upperValue + "/" + lowValue;
+        }
+
+        /// <summary>
+        /// Функция упрощения дроби
+        /// </summary>
+        /// <param name="x">Входящее значение дроби</param>
+        /// <returns></returns>
+        private static Fraction Simple(Fraction x)
+        {
+            Fraction y = new Fraction();
+            bool flag = false;
+
+            while (true)
+            {
+                if (x.upperValue % 2 == 0 && x.lowValue % 2 == 0) { x.upperValue = x.upperValue / 2; x.lowValue = x.lowValue / 2; flag = true; }
+                if (x.upperValue % 3 == 0 && x.lowValue % 3 == 0) { x.upperValue = x.upperValue / 3; x.lowValue = x.lowValue / 3; flag = true; }
+                if (x.upperValue % 5 == 0 && x.lowValue % 5 == 0) { x.upperValue = x.upperValue / 5; x.lowValue = x.lowValue / 5; flag = true; }
+                if (x.upperValue % 7 == 0 && x.lowValue % 7 == 0) { x.upperValue = x.upperValue / 7; x.lowValue = x.lowValue / 7; flag = true; }
+
+                if (flag == false)
+                {
+                    break;
+                }
+            }
+
+            y.upperValue = x.upperValue;
+            y.lowValue = x.lowValue;
+            return y;
+        }
+
+
+        /// <summary>
+        /// Суммирование дробей
+        /// </summary>
+        /// <param name="x">Слагаемое с которым происходит операциия суммирования</param>
+        /// <returns></returns>
+        public Fraction Summ(Fraction x)
+        {
+            Fraction y = new Fraction();
+
+            //Приведение к общему знаменателю
+            if (lowValue != x.lowValue && lowValue < x.lowValue)
+            {
+                int tempVal = lowValue;
+                lowValue *= x.lowValue;
+                x.lowValue *= tempVal;
+                upperValue *= x.lowValue;
+                x.upperValue *= tempVal; 
+
+            }
+            else if (lowValue != x.lowValue && lowValue > x.lowValue)
+            {
+                int tempVal = x.lowValue;
+                x.lowValue *= lowValue;
+                lowValue *= tempVal;
+                x.upperValue *= lowValue;
+                upperValue *= tempVal;
+            }
+
+            y.upperValue = upperValue + x.upperValue;
+            y.lowValue = lowValue;
+
+            y = Fraction.Simple(y);
+
+            return y;
+        }
+
+        /// <summary>
+        /// Вычитание дроби
+        /// </summary>
+        /// <param name="x">Вычитаемая дробь</param>
+        /// <returns></returns>
+        public Fraction Minus(Fraction x)
+        {
+            Fraction y = new Fraction(); ;
+
+            //Приведение к общему знаменателю
+            if (lowValue != x.lowValue && lowValue < x.lowValue)
+            {
+                int tempVal = lowValue;
+                lowValue *= x.lowValue;
+                x.lowValue *= tempVal;
+                upperValue *= x.lowValue;
+                x.upperValue *= tempVal;
+
+            }
+            else if (lowValue != x.lowValue && lowValue > x.lowValue)
+            {
+                int tempVal = x.lowValue;
+                x.lowValue *= lowValue;
+                lowValue *= tempVal;
+                x.upperValue *= lowValue;
+                upperValue *= tempVal;
+            }
+
+            y.upperValue = upperValue - x.upperValue;
+            y.lowValue = lowValue;
+
+            y = Fraction.Simple(y);
+
+            return y;
+        }
+
+        /// <summary>
+        /// Произведение дробей
+        /// </summary>
+        /// <param name="x">Дробь на которую происходит умножение исходной дроби</param>
+        /// <returns></returns>
+        public Fraction Multiple(Fraction x)
+        {
+            Fraction y = new Fraction(); ;
+
+            y.upperValue = upperValue * x.upperValue;
+            y.lowValue = lowValue * x.lowValue;
+
+            y = Fraction.Simple(y);
+
+            return y;
+        }
+
+        /// <summary>
+        /// Деление дробей
+        /// </summary>
+        /// <param name="x">Дробь на которую происходит деление</param>
+        /// <returns></returns>
+        public Fraction Division(Fraction x)
+        {
+            Fraction y = new Fraction(); ;
+
+            y.upperValue = upperValue * x.lowValue;
+            y.lowValue = lowValue * x.upperValue;
+
+            y = Fraction.Simple(y);
+
+            return y;
+        }
+    }
+}
---
+++ Lecture3HomeWork/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Lecture3HomeWorkClasses;
+
+namespace Lecture3HomeWork
+{
+    class Program
+    {
+        private static char Choice()
+        {
+            ConsoleKeyInfo selectedTask;
+            var rule = @"[1-4]";
+
+            Console.WriteLine("Выберите операцию которую хотите провести:");
+            Console.WriteLine("\t1. Сложение");
+            Console.WriteLine("\t2. Вычитание");
+            Console.WriteLine("\t3. Умножение");
+            Console.WriteLine("\t4. Деление");
+            selectedTask = Console.ReadKey(true);
+            if (Regex.IsMatch(selectedTask.KeyChar.ToString(), rule))
+            {
+                return selectedTask.KeyChar;
+            }
+            else
+            {
+                return '0';
+            }
+        }
+
+        public static void TaskN1()
+        {
+            Complex x;
+            Complex y;
+
+            try
+            {
+                Console.Write("Введите действительную часть первого значения: ");
+                x.im = int.Parse(Console.ReadLine());
+                Console.Write("Введите комплексную часть первого значения: ");
+                x.re = int.Parse(Console.ReadLine());
+                Console.Write("Введите действительную часть второго значения: ");
+                y.im = int.Parse(Console.ReadLine());
+                Console.Write("Введите комлпексную часть второго значения: ");
+                y.re = int.Parse(Console.ReadLine());
+
+                switch (Choice())
+                {
+                    case '1':
+                        x = x.Plus(y);
+                        Console.WriteLine("Результат: " + x.ToString());
+                        break;
+                    case '2':
+                        x = x.Minus(y);
+                        Console.WriteLine("Результат: " + x.ToString());
+                        break;
+                    case '3':
+                        x = x.Multi(y);
+                        Console.WriteLine("Результат: " + x.ToString());
+                        break;
+                    case '4':
+                        x = x.Division(y);
+                        Console.WriteLine("Результат: " + x.ToString());
+                        break;
+                    default:
+                        Console.WriteLine("Введено некорректное значение!");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+            }
+        }
+
+        public static void TaskN2()
+        {
+            Task2.InputValuesUntilNotZero();
+        }
+
+        public static void TaskN3()
+        {
+            Fraction x = new Fraction(); ;
+            Fraction y = new Fraction();;
+
+            try
+            {
+                Console.Write("Введите числитель первой дроби: ");
+                x.upperValue = int.Parse(Console.ReadLine());
+                Console.Write("Введите знаменатель первой дроби: ");
+                x.lowValue = int.Parse(Console.ReadLine());
+                Console.Write("Введите числитель второй дроби: ");
+                y.upperValue = int.Parse(Console.ReadLine());
+                Console.Write("Введите знаменатель второй дроби: ");
+                y.lowValue = int.Parse(Console.ReadLine());
+
+                switch (Choice())
+                {
+                    case '1':
+                        x = x.Summ(y);
+                        Console.WriteLine("Результат: " + x.ToString());
+                        break;
+                    case '2':
+                        x = x.Minus(y);
+                        Console.WriteLine("Результат: " + x.ToString());
+                        break;
+                    case '3':
+                        x = x.Multiple(y);
+                        Console.WriteLine("Результат: " + x.ToString());
+                        break;
+                    case '4':
+                        x = x.Division(y);
+                        Console.WriteLine("Результат: " + x.ToString());
+                        break;
+                    default:
+                        Console.WriteLine("Введено некорректное значение!");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo selectedTask;
+            var rule = @"[1-7]";
+
+            Console.WriteLine("Добрый день, уважаемый пользователь. Демонстрацию работы какой программы вы бы хотели увидеть?");
+            Console.WriteLine("\t1. Программа проведения арифметических операций с комплексными числами.");
+            Console.WriteLine("\t2. Программа подсчета суммы всех нечетных положительных чисел введенных пользователем.");
+            Console.WriteLine("\t3. Программа проведения арифметических операций с дробями.");
+            selectedTask = Console.ReadKey(true);
+            if (Regex.IsMatch(selectedTask.KeyChar.ToString(), rule))
+            {
+                switch (selectedTask.KeyChar)
+                {
+                    case '1':
+                        TaskN1();
+                        break;
+                    case '2':
+                        TaskN2();
+                        break;
+                    case '3':
+                        TaskN3();
+                        break;
+                }
+            }
+        }
+    }
+}
